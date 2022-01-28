1bd23031b840845fafea1da4ae379882745ba0cd
ShadowCoderVM <46918487+5angar1us@users.noreply.github.com>
2022-01-28 22:11:55 +0300

    Updating to .Net 6

--- BondingCodeFiles/Program.cs
+++ BondingCodeFiles/Program.cs
@@ -1,122 +1,91 @@
-﻿using System;
+﻿using CommandLine;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using HooverUnlimited.DotNetRtfWriter;
+using System.Text;
 
 namespace BondingCodeFiles
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            MainWork();
 
-            Console.ReadKey();
+        public class Options
+        {
+            [Option('i', "infile", Required = true, HelpText = "")]
+            public string sourcePath { get; set; }
+
+            [Option('o', "outfile", Required = true, HelpText = "")]
+            public string targetPath { get; set; }
         }
 
-        private static void MainWork()
+        static async Task<int> Main(string[] args)
         {
-            string folderPath = $@"C:\1.Developing\С#\ИнстрСредства ИС\PrimaryDataAnalysis7";
-            string outputFileName = "Code of progect.rtf";
-            string outputPath = Path.Combine(folderPath, outputFileName);
+            return await Parser.Default.ParseArguments<Options>(args)
+                .MapResult(async (Options opts) =>
+                {
+                    return Run(opts.sourcePath, opts.targetPath);
+                },
+                errs => Task.FromResult(-1)); // Invalid arguments
+        }
+
+        private static int Run(string sourcePath, string targetPath)
+        {
+            if (Directory.Exists(sourcePath) == false)
+            {
+                Console.WriteLine("This path does not exist");
+                return -1;
+            }
+
             string extensionPattern = "*.cs";
 
             List<string> ignoreList = new List<string>()
             {
-                @"\Debug\",
+                "Debug",
                 "Properties",
-                ".Designer.",
+                "Designer",
+                "AssemblyAttributes"
             };
 
-            var files = Directory.GetFiles(folderPath, extensionPattern, SearchOption.AllDirectories).ToList();
+            var files = Directory.GetFiles(sourcePath, extensionPattern, SearchOption.AllDirectories).ToList();
 
             files.RemoveAll(file => ignoreList.Exists(ignore => file.Contains(ignore)));
 
-            WriteFile(files, outputPath);
+            WriteFiles(files, targetPath);
+
+            return 0;
         }
 
-        private static void ConsoleWrite(string folderPath, List<string> files)
+        private static void WriteFiles(List<string> files, string outputPath)
         {
-            foreach (var s in files)
-            {
-                string relativePath = GetRelativePath(folderPath, s);
-                Console.WriteLine(relativePath);
-            }
-        }
-
-        private static void WriteFile(List<string> files, string outputPath)
-        {
-            var doc = new RtfDocument(PaperSize.A4, PaperOrientation.Landscape, Lcid.English);
-
-            // Create fonts and colors for later use
-            var times = doc.CreateFont("Times New Roman");
-            var courier = doc.CreateFont("Courier New");
+            var sb = new StringBuilder();
 
             foreach (var s in files)
             {
                 using (var sr = new StreamReader(s))
                 {
-                   
+
                     var fileName = $"Файл \"{Path.GetFileName(s)}\"";
                     var text = sr.ReadToEnd();
-                    
-                    addTitle(doc, times, fileName);
-                    addText(doc, times, text);
 
+                    sb.AppendLine(CreateTitle(fileName));
+                    sb.AppendLine(text);
                 }
             }
-            doc.Save(outputPath);
-
-
-            var p = new Process { StartInfo = { FileName = outputPath } };
-            p.Start();
-        }
-
-        private static void addTitle(RtfDocument doc, FontDescriptor times, string title)
-        {
-            RtfCharFormat fmt = null;
-
-            RtfParagraph par = doc.AddParagraph();
-
-            par.StartNewPage = true;
-
-            par.DefaultCharFormat.Font = times;
-            par.Alignment = Align.Center;
-            par.SetText(title);
-
-            fmt = par.AddCharFormat(0, par.Text.Length - 1);
-            fmt.FontSize = 14;
-
-            fmt = par.AddCharFormat(0, 4);
-            fmt.FontStyle.AddStyle(FontStyleFlag.Bold);
-            fmt.FontSize = 14;
-        }
-
-        private static void addText(RtfDocument doc, FontDescriptor times, string text)
-        {
-            RtfCharFormat fmt = null;
-
-            RtfParagraph par = doc.AddParagraph();
-
-            par.DefaultCharFormat.Font = times;
-            par.Alignment = Align.Left;
-            par.SetText(text);
-
-            fmt = par.AddCharFormat(0, par.Text.Length - 1);
-            fmt.FontSize = 14;
-        }
-
-        private static string GetRelativePath(string relativeTo, string path)
-        {
-            var uri = new Uri(relativeTo);
-            var rel = Uri.UnescapeDataString(uri.MakeRelativeUri(new Uri(path)).ToString()).Replace(Path.AltDirectorySeparatorChar, Path.DirectorySeparatorChar);
-            if (rel.Contains(Path.DirectorySeparatorChar.ToString()) == false)
+            
+            using (var sw = new StreamWriter(outputPath))
             {
-                rel = $".{ Path.DirectorySeparatorChar }{ rel }";
+                sw.WriteLine(sb.ToString());
             }
-            return rel;
+
+            
+        }
+
+        private static string CreateTitle(string titleText)
+        {
+            const string titleSeparator = "========";
+            return $"{titleSeparator} {titleText} {titleSeparator}";
         }
     }
 }
